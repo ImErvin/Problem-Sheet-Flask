584dd78f3e2f5f6ed41bf3bf25ecd0d53b53484a
Ervin Mamutov <Ervin Mamutov>
2016-10-04 20:08:45 +0100

    Step 4 complete

--- hello.py
+++ hello.py
@@ -1,7 +1,13 @@
 from flask import Flask 
 app = Flask(__name__)  
+
 @app.route("/") 
 def hello():     
-	return "Hello World!"  
+	return "Hello!" 
+	
+@app.route('/name/<string:name>')
+def name(name):
+    return 'Your name is %s' %name
+	
 if __name__ == "__main__":     
 	app.run()
