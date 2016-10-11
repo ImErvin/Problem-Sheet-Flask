03ce4a3d16307e5c51651402c689722427072cc7
Ervin Mamutov <Ervin Mamutov>
2016-10-11 17:26:12 +0100

    Attempt using jquery and javascript for step 5

--- hello.py
+++ hello.py
@@ -1,11 +1,18 @@
 from flask import Flask 
-app = Flask(__name__)  
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+CORS(app)
 
 @app.route("/") 
 def hello():     
 	return "Hello!" 
+
+@app.route('/name')
+def enterName():
+	return "Please specify your name in the address bar e.g /name/ervin"
 	
-@app.route('/name/<string:name>', methods=['GET'])
+@app.route('/name/<string:name>')
 def name(name):
     return 'Your name is %s' %name
 	
